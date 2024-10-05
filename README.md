46c3deec161a7bec0347a8c0259f18335e9cc0ff
danjys <danjys@gmail.com>
2024-10-05 15:47:42 -0400

    Enable speech recognition using SpeechRecognition Library

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 pip install --upgrade pip
 pip install pyobjc
 pip install pyttsx3
+pip install SpeechRecognition
+pip install PyAudio
--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init()
@@ -10,4 +10,36 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("I Am JARVIS... how are you doing DJ")    
+
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening...')
+        r.pause_threshold = 1
+        r.energy_threshold = 300  # You can adjust this based on your environme
+        r.adjust_for_ambient_noise(source)
+
+        try:
+            audio = r.listen(source, phrase_time_limit=6)
+        except sr.WaitTimeoutError:
+            print("Timeout error, please try again.")
+            return ""
+    
+    try:
+        print('recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand the audio.")
+        return ""
+    except sr.RequestError:
+        print("Could not request results from Google Speech Recognition service.")
+        return ""
+    
+    return query.lower()
+
+text = takecommand()
+
+speak(text)    
