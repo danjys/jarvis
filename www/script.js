65eefcb030d58cebe55eced9da40baa20d6779ee
danjys <danjys@gmail.com>
2024-10-05 09:30:12 -0400

    changes

--- www/script.js
+++ www/script.js
@@ -72,9 +72,9 @@ function canvasApp() {
 		numToAddEachFrame = 8;
 
 		//particle color
-		r = 0;
-		g = 72;
-		b = 255;
+		r = 181;
+		g = 102;
+		b = 247;
 
 		rgbString = "rgba(" + r + "," + g + "," + b + ","; //partial string for color which will be completed by appending alpha value.
 		particleAlpha = 1; //maximum alpha
